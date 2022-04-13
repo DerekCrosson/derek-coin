1d7d5e2821b1e5c777b77b7e52d2e0eef9660a45
Derek Crosson <derekcrosson18@gmail.com>
2022-04-13 03:25:58 +0200

    refacor: hard code name and symbol for simplicity

--- contracts/DerekCoinToken.sol
+++ contracts/DerekCoinToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DerekCoinToken is ERC20 {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    constructor() ERC20("Derek Coin", "DEREK") {
         _mint(msg.sender, 10 * 10 ** 18);
     }
 }
